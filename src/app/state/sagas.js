01c7f84c28f3eea8d310d155228d5249d5f1db64
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2020-04-13 14:35:57 +0530

    saga with cancel

--- src/app/state/sagas.js
+++ src/app/state/sagas.js
@@ -5,14 +5,29 @@
     // workflow
     // handy, spawn, fork, sequential/parellel/all/wait all/reject all
 
-import {takeEvery, put, call } from 'redux-saga/effects';
+import {takeEvery, put, call, 
+        takeLatest, cancelled
+        } from 'redux-saga/effects';
 
 import * as ActionTypes from './action-types';
 import * as actions from './actions';
 import * as service from './service';
 
+import axios from 'axios';
+
+
 // takeEvery - is interceptor for the action, wait for action type
 // when an action with type dispatched, it execute a callback function
+
+// takeEvery 
+// you make 10 calls to getBrands continousely, 
+// takeEvery execute all the calls from begining to end
+
+
+//takeLatest, same like takeEvery all aspects, except 
+// if two same actions initiated, if the first is not completed
+// then it cancel the first one and execute the second one which is latest
+// takeLatest => 
 
 // put - is a dispatcher
 
@@ -45,18 +60,34 @@ export function* counterSaga() {
 
 function* fetchBrands(action) {
     console.log('fetching brands', action);
+    const source = axios.CancelToken.source();
 
-    //loading state to true
-    yield put(actions.initializeLoading(true));
-    const brands = yield call(service.getBrands)// no argument for getBrands
-    // initialize brand in store
-    yield put(actions.initializeBrands(brands));
-    // loading state to be false
-    yield put(actions.initializeLoading(false));
+    try {
+        //loading state to true
+        yield put(actions.initializeLoading(true));
+        const brands = yield call(service.getBrands, {cancelToken: source.token} )// 1 argument for getBrands
+        // initialize brand in store
+        yield put(actions.initializeBrands(brands));
+        // loading state to be false
+        yield put(actions.initializeLoading(false));
+    }catch(error) {
+        console.log('error in execution', error)
+    }
+    finally {
+        if (yield cancelled()) {
+           console.log('*****cancelled the call, cancel pending apis calls');
+           source.cancel('task/api cancelled');
+        }
+        console.log('finally block')
+    }
 }
  
 // importe in store.js, should be part of saga.run(fetchBrandSaga)
 export function* fetchBrandSaga() {
-    yield takeEvery(ActionTypes.REQUEST_BRANDS, fetchBrands)
+    // yield takeEvery(ActionTypes.REQUEST_BRANDS, fetchBrands)
+
+    // cancel previous effect/tasks if any, execute only the last one effect
+    // BEWARE: API calls not cancelled, we need workaround
+    yield takeLatest(ActionTypes.REQUEST_BRANDS, fetchBrands)
 }
 
--- src/app/state/service.js
+++ src/app/state/service.js
@@ -4,8 +4,8 @@
 import axios from 'axios';
 import config from '../config';
 
-export const getBrands = () => {
-    return axios.get(`${config.apiEndPoint}/api/brands`)
+export const getBrands = (options = {}) => {
+    return axios.get(`${config.apiEndPoint}/api/brands`, options)
            .then (response => {
                //returned to caller promise
                // data is list of brand object
